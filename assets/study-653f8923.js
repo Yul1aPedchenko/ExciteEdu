d5f90709738d0a34a85632e566b419cb77d908ec
Ostik1220 <117195748+Ostik1220@users.noreply.github.com>
2024-07-18 19:01:55 +0000

    Deploying to gh-pages from @ Yul1aPedchenko/ExciteEdu@6d665dd8361f3b284ec7ab9110d80d6935494424 🚀

---
+++ assets/study-653f8923.js
@@ -0,0 +1,1 @@
+import"./mobile-menu-90ab93cd.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
--- assets/study-72acf5a5.js
+++
@@ -1,1 +0,0 @@
-import"./mobile-menu-d434723d.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
