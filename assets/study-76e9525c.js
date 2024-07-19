7b10f1b436ba45a5b8da4abc98360375e0cd4b67
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2024-07-19 10:19:25 +0000

    Deploying to gh-pages from @ Yul1aPedchenko/ExciteEdu@934bdf8f2687caaf6c0752499cab42b2d18f8798 🚀

--- assets/study-76e9525c.js
+++
@@ -1,1 +0,0 @@
-import"./mobile-menu-f3a0f5f3.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
---
+++ assets/study-96fe55c8.js
@@ -0,0 +1,1 @@
+import"./mobile-menu-4cc68a6e.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
