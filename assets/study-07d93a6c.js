1d48abd4994359eda6dedf491343ac6ac00f1271
pypuc <145868278+pypuc@users.noreply.github.com>
2024-07-21 07:42:37 +0000

    Deploying to gh-pages from @ Yul1aPedchenko/ExciteEdu@4547fbf38c508c509814e2f46aa34e94f483e101 🚀

--- assets/study-07d93a6c.js
+++
@@ -1,1 +0,0 @@
-import"./mobile-menu-80d9d880.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
---
+++ assets/study-491a902f.js
@@ -0,0 +1,1 @@
+import"./mobile-menu-3c86e81c.js";const o=document.querySelectorAll(".topic__question");o.forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("open")})});
