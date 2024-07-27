5262bf173fcbb1dae68033a014ac99d0fa808d43
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-07-27 13:18:20 +0300

    problem

--- src/themes.js
+++ src/themes.js
@@ -1,12 +1,12 @@
 const product = [
   {
     id: 0,
-    image: './img/themes_page/physics.png',
+    image: './img/themes/physics.png',
     title: 'Definition of physics and its role in science',
   },
   {
     id: 1,
-    image: './img/themes_page/themes/physics-2.png',
+    image: './img/themes/physics-2.png',
     title: 'Basic physical quantities and units of measurement',
   },
   {
