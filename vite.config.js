0764d2023ebf96c14c65562b9108fc80dda3ad64
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2024-07-27 17:46:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
+const HOST_URL = process.env.HOST_URL || 'http://localhost';
+
 export default defineConfig({
   define: {
     global: {},
+    'process.env.HOST_URL': JSON.stringify(HOST_URL),
   },
   root: 'src',
   build: {
@@ -16,3 +19,4 @@ export default defineConfig({
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
+  
