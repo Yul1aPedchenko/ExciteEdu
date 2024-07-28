de494f5ddadaac3baa145a7151d513139728da54
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2024-07-28 10:37:28 +0000

    Deploying to gh-pages from @ Yul1aPedchenko/ExciteEdu@8aac7cd152fc873856a65f5e32dcb39f41b0844f 🚀

--- assets/index-c902946b.js
+++
@@ -1,1 +0,0 @@
-import"./modal-signup-89765b79.js";new Swiper(".swiper-container",{loop:!0,loopedSlides:2,grabCursor:!0,slidesPerView:1,spaceBetween:20,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{768:{slidesPerView:2},1200:{slidesPerView:2}}});
