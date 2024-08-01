d1e6b221f5fd5a55554cf042f529d99ef5d85fb8
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2024-08-01 11:33:53 +0000

    Deploying to gh-pages from @ Yul1aPedchenko/ExciteEdu@e736e801f1c63ecbfebbd24f2383630b29c75a5c 🚀

--- assets/self-96ff524c.js
+++
@@ -1,1 +0,0 @@
-import"./modal-signup-b7b1dc1b.js";document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("load-more"),n=document.querySelectorAll(".resources__item"),o=Array.from(n).slice(window.innerWidth>=1200?3:2);e.addEventListener("click",function(){o.forEach(t=>{t.classList.remove("hidden"),t.style.display="block"}),e.style.display="none"})});
