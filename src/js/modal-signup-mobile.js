638796ce24c8c569eb24715feaf98b232cbe3443
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-08-03 21:51:17 +0300

    solve the problem

--- src/js/modal-signup-mobile.js
+++ src/js/modal-signup-mobile.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[signup-modal-mobile-open]'),
-    closeModalBtn: document.querySelector('[signup-modal-close]'),
+    closeModalBtn: document.querySelector('[signup-modal-mobile-close]'),
     modal: document.querySelector('[signup-modal-mobile]'),
   };
 
