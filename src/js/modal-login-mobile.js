a5c34f2830cf4b51bd3805a8d3217f6f34d40020
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-08-03 22:01:30 +0300

    solve the problem

--- src/js/modal-login-mobile.js
+++ src/js/modal-login-mobile.js
@@ -1,15 +1,32 @@
-(() => {
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[login-modal-mobile-open]'),
+//     closeModalBtn: document.querySelector('[login-modal-mobile-close]'),
+//     modal: document.querySelector('[login-modal-mobile]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     document.body.classList.toggle('no-scroll');
+//   }
+// })();
+document.addEventListener('DOMContentLoaded', () => {
   const refs = {
     openModalBtn: document.querySelector('[login-modal-mobile-open]'),
     closeModalBtn: document.querySelector('[login-modal-mobile-close]'),
     modal: document.querySelector('[login-modal-mobile]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
   }
-})();
+});
